85d7b9401e1ca202c4e85024735e2175c266a5e3
Jaqueline Lemes Botaro <jlemesbotaro@gmail.com>
2020-04-17 10:27:02 -0300

    Use Ui nav titles and system colors

--- SuperM/HomeViewController.swift
+++ SuperM/HomeViewController.swift
@@ -9,19 +9,10 @@
 import UIKit
 
 // https://developer.apple.com/documentation/uikit/uinavigationcontroller
-class HomeController: UINavigationController {
-    private lazy var menuText: UILabel = {
-            let menuText = UILabel()
-            menuText.textColor = UIColor.purple
-            menuText.text = "My Supermarket"
-            menuText.textAlignment = .center
-            menuText.translatesAutoresizingMaskIntoConstraints = false
-            return menuText
-    }()
-    
-    private lazy var emptyText: UILabel = {
+class HomeViewController: UIViewController {
+  private lazy var emptyText: UILabel = {
         let emptyText = UILabel()
-        emptyText.textColor = UIColor.systemGray4
+        emptyText.textColor = .systemGray4
         emptyText.text = "Add a list to start"
         emptyText.textAlignment = .center
         emptyText.font = UIFont.systemFont(ofSize: 24)
@@ -32,7 +23,7 @@ class HomeController: UINavigationController {
     private lazy var addButton: UIButton = {
         let addButton = UIButton()
         addButton.setTitle("Add List", for: .normal)
-        addButton.setTitleColor(.purple, for: .normal)
+        addButton.setTitleColor(.systemPurple, for: .normal)
         addButton.translatesAutoresizingMaskIntoConstraints = false
         return addButton
     }()
@@ -40,30 +31,29 @@ class HomeController: UINavigationController {
     private lazy var loginButton: UIButton = {
         let loginButton = UIButton()
         loginButton.setTitle("Login", for: .normal)
-        loginButton.setTitleColor(.purple, for: .normal)
+        loginButton.setTitleColor(.systemPurple, for: .normal)
         loginButton.translatesAutoresizingMaskIntoConstraints = false
         return loginButton
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "My Supermarket"
+        navigationItem.largeTitleDisplayMode = .automatic
         setupUI()
     }
 }
 
-extension HomeController {
+extension HomeViewController {
     private func setupUI() {
-        view.backgroundColor = UIColor.white
+        view.backgroundColor = .systemBackground
         
-        view.addSubview(menuText)
         view.addSubview(emptyText)
         view.addSubview(addButton)
         view.addSubview(loginButton)
  
         NSLayoutConstraint.activate([
-            menuText.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            menuText.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            menuText.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
             emptyText.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             emptyText.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             addButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
