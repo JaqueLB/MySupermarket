9d0d9f90e340bd25d1accbb5c68b6d5f9e9efa8b
Jaqueline Lemes Botaro <jlemesbotaro@gmail.com>
2020-06-05 10:15:58 -0300

    Clean controller and implement new cell

--- SuperM/Features/Home/View/Controller/HomeTableViewController.swift
+++ SuperM/Features/Home/View/Controller/HomeTableViewController.swift
@@ -9,92 +9,44 @@
 import UIKit
 
 class HomeTableViewController: UITableViewController {
-    var listOfLists = [GroceryItem]()
-    var listOfProd = ["café da manhã", "terça-feira"]
+    private var listCollection = [GroceryItem]()
+    private var dataSource: UITableViewDiffableDataSource<SectionListCollection, GroceryItem>?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // criar nova clase de cell para fazer a cell customizada
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.title = "My Supermarket"
-        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(buttonTapped))
-        // chamadas async, fetch de banco de dados, na primeira vez a table view vem vazia, e após terminar a chamada do bd, damos um reload para exibir
-        tableView.reloadData()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        setupNavigation()
+        setupTableView()
+        setupDataSource()
     }
 
-    @objc func buttonTapped() {
+    @objc func addButtonTapped() {
         print("oi")
     }
 
-    // MARK: - Table view data source
-    // separações dentro da lista
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
+    func setupNavigation() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "My Supermarket"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(addButtonTapped))
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return listOfLists.count
+    func setupTableView() {
+        tableView.register(HomeTableViewCell.self, forCellReuseIdentifier: HomeTableViewCell.reuseIdentifier)
+    }
+}
+
+extension HomeTableViewController {
+    func setupDataSource() {
+        dataSource = UITableViewDiffableDataSource<SectionListCollection, GroceryItem>(tableView: tableView, cellProvider: {
+            (tableView, indexPath, groceryItem) -> HomeTableViewCell? in
+            let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.reuseIdentifier, for: indexPath) as! HomeTableViewCell
+            return cell
+        }
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
-        cell.textLabel?.text = listOfProd[indexPath.row]
-
-        return cell
+    func snapshot(from listCollection: [GroceryItem]) {
+        var snapshot = NSDiffableDataSourceSnapshot<SectionListCollection, GroceryItem>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(listCollection, toSection: .main)
+        dataSource?.apply(snapshot, animatingDifferences: true)
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
